f97534cf4cb549a1331a4475120f2af14875bd56
Garcia Joaquin <138274616+joaq-dev@users.noreply.github.com>
2024-11-14 16:13:13 +0800

    Update train_model.sh
    Changed to local execution and selected which GPUs to use

--- train_model.sh
+++ train_model.sh
@@ -1,1 +1,1 @@
-python main.py --mode train  --data_dir "data/focus_stack_dataset/dataset"  --epochs 1000
+CUDA_VISIBLE_DEVICES=0,6 python main.py --mode train --local --data_dir "data/focus_stack_dataset/dataset"  --epochs 1000
