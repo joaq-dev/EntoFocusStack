1373ab4472dfc3b63796fceb72fad3cc604c5489
Garcia Joaquin <138274616+joaq-dev@users.noreply.github.com>
2025-01-27 16:15:05 +0800

    Update train_model.sh

--- train_model.sh
+++ train_model.sh
@@ -1,1 +1,1 @@
-torchrun --nproc_per_node=2  main.py --mode train --local --data_dir "data/focus_stack_dataset/dataset"  --epochs 1000
+torchrun --nproc_per_node=2  main.py --mode train --data_dir "data/focus_stack_dataset/dataset"  --epochs 1000
