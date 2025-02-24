7debe5acde42738e9555c0c31afd17ebdc8e3e65
Joaquin Garcia <138274616+joaq-dev@users.noreply.github.com>
2025-02-24 16:39:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
   
   # cluster settings
   parser_cluster = parser.add_argument_group("cluster", "Cluster configurations")
-  parser_cluster.add_argument("--ngpus", type=int, default=2, help="Number of GPUs to use.")
+  parser_cluster.add_argument("--ngpus", type=int, default=1, help="Number of GPUs to use.")
   #parser_cluster.add_argument("--gpus_to_use", type=int, nargs='+', default=[0, 4], help="List of GPUs to use (e.g., 0 1 4 for GPUs 0, 1, and 4). Default is [0, 4].")
 
   # project settings
