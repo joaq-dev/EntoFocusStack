b41cadf36cc4bb50ec9aa5768a55804d7a147f14
Joaquin Garcia <138274616+joaq-dev@users.noreply.github.com>
2025-02-24 16:49:05 +0800

    Update train_model.sh

--- train_model.sh
+++ train_model.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,4 torchrun --nproc_per_node=2  main.py --mode train --data_dir data  --epochs 1000 --n_colors 3
+CUDA_VISIBLE_DEVICES=0 python main.py --mode train --data_dir data  --epochs 1000 --n_colors 3 
