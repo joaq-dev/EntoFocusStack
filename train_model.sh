06d32098f8355240bf33b2cd0f8b42834e91583b
Joaquin Garcia <138274616+joaq-dev@users.noreply.github.com>
2025-02-24 16:49:49 +0800

    Update train_model.sh

--- train_model.sh
+++ train_model.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --mode train --data_dir data  --epochs 1000 --n_colors 3 
+CUDA_VISIBLE_DEVICES=0 python main.py --mode train --data_dir data  --epochs 1000 --n_colors 3 --ngpus 1
